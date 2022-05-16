4dfbbf0b188ef99189d9544b99c197c74441dae6
Ewan Gilchrist <ewan@gilchrist.scot>
2022-05-16 13:26:49 +0000

    Initail commit of compose structure

--- cogip-src/database/connection.php
+++ cogip-src/database/connection.php
@@ -2,9 +2,9 @@
 
 function dbconnect(){
 
-    $hostname = "localhost";
-    $usernamedb = "root";
-    $passworddb = "root";
+    $hostname = "db";
+    $usernamedb = "cogip";
+    $passworddb = "ohv9vee1joleepa7eepudaitho6aecae6uXee0Juataeth7koocie0ahva7po5la";
     $dbname = "cogip";
 
     $conn = mysqli_connect($hostname, $usernamedb, $passworddb, $dbname) or die ("unable to connect");
