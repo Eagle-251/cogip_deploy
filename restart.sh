4c8d6864ea349aaad069c6c7efb6a65a51af1f7a
Ewan Gilchrist <ewan@gilchrist.scot>
2022-05-17 08:44:03 +0000

    Removed testing files

--- restart.sh
+++
@@ -1,5 +0,0 @@
-docker-compose down
-sudo rm -r db
-docker-compose up -d
-#sleep 6
-#docker exec -it cogip_deploy-db-1 sh -c 'mysql -uroot -p cogip --password=EiZanuo6geimeeR3Fai9ahbalee1TieShohtahz4geeY0AhFooHo0ox6oi6eeb2k'
